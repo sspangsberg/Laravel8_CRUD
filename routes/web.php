f64ffa2d12f364f2c4fa8f811aae422e5eb7ffc3
sms_easv <smsj@easv.dk>
2020-12-31 12:07:28 +0100

    Complete CRUD

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('products', ProductController::class);
